c73b65038db09723d5e9e2ccb8635afed7377454
Andrew Beal <ABeal@whoi.edu>
2012-06-11 13:49:55 -0700

    whoi-fpga: add new register definitions, add version data to boot message
    (cherry picked from commit 458f3f0ce144d869b83a4fb08cfe1006245d7a33)

--- drivers/gpio/whoifpga_gpio.c
+++ drivers/gpio/whoifpga_gpio.c
@@ -29,8 +29,37 @@
 
 static DEFINE_SPINLOCK(gpio_lock);
 
-#define IO_CTRL_BASE	(0x42)
-#define IO_CTRL_STATUS	(0x62)
+/*
+ * Map the Version API Registers so the driver can load them at initialization time.
+ */
+
+#define FPGA_API_LEVEL (0x00) //0x00
+#define FPGA_VER_MAJOR (0x02) //0x01
+#define FPGA_VER_MILE  (0x04) //0x02
+#define FPGA_VER_MINOR (0x06) //0x03
+#define FPGA_VER_DEVEL (0x08) //0x04
+#define FPGA_VER_FLAG  (0x0A) //0x05
+
+/*
+ * Define the Magic Number registers for Verification of Hardware
+ */
+#define MAGIC1	       (0x20) //0x010
+#define MAGIC2	       (0x22) //0x011
+
+/*
+ * Define the Fixed Hibernate Register
+ */
+#define HIBERNATE	(0x2A) //0x15
+
+/*
+ * Map to the Ctrl Register in the FPGA
+ */
+#define IO_CTRL_BASE	(0x42) //0x21
+
+/*
+ * Map to the Status Register in the FPGA
+ */
+#define IO_CTRL_STATUS	(0xC2) //0x61
 
 void __iomem *fpga_base;
 
@@ -116,6 +145,10 @@ static int __devinit whoifpga_gpio_probe(struct platform_device *pdev)
 {
 	struct whoifpga_platform_data *pdata;
 	int err;
+	u16 api, major, minor, mile, devel;
+	char flag;
+	u16 magic1, magic2;
+	char version_string[26];
 
 	pdata = pdev->dev.platform_data;
 	if (!pdata || !pdata->gpio_base || !pdata->fpga_base_address) {
@@ -138,7 +171,34 @@ static int __devinit whoifpga_gpio_probe(struct platform_device *pdev)
 	if (err < 0)
 		goto err_whoifpga_gpio;
 
-	dev_info(&pdev->dev, "FPGA at 0x%08x, %d GPIO's based at %d\n",
+	//Lets check the magic numbers
+	magic1 = __raw_readw(fpga_base + MAGIC1);
+	magic2 = __raw_readw(fpga_base + MAGIC2);
+	if (magic1 != magic2)
+		goto err_whoifpga_gpio;
+
+	//Let's Read the Current Version info of the FPGA API.
+	api = __raw_readw(fpga_base + FPGA_API_LEVEL);
+	major = __raw_readw(fpga_base + FPGA_VER_MAJOR);
+	mile = __raw_readw(fpga_base + FPGA_VER_MILE);
+	minor = __raw_readw(fpga_base + FPGA_VER_MINOR);
+	devel = __raw_readw(fpga_base + FPGA_VER_DEVEL);
+	flag = (char)__raw_readw(fpga_base + FPGA_VER_FLAG);
+
+	//Print Version information
+	if(flag != '\0')
+	{
+		snprintf(version_string, sizeof(version_string),"%d.%d.%d.%d-%c",
+			 major,mile,minor,devel,flag);
+	}
+	else
+	{
+		snprintf(version_string, sizeof(version_string),"%d.%d.%d.%d",
+			 major,mile,minor,devel);
+	}
+
+	//Print Info about the Location of the GPIO base
+	dev_info(&pdev->dev, "WHOI FPGA(Version %s) at 0x%08x, %d GPIO's based at %d\n", version_string,
 		pdata->fpga_base_address, WHOIFPGA_NR_GPIOS, whoifpga_gpio.base);
 
 	return 0;
